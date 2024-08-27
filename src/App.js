aa12a893631837372c2a1d3f1d042ef717a3a409
Sudhanshu2520 <sudhanshulokhande768@gmail.com>
2024-08-28 03:02:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Sidebar from './components/Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -19,7 +18,6 @@ const App = () => {
         <Routes>
           <Route path="/add" element={<Add />} />
           <Route path="/list" element={<List />} />
-          <Route path="/orders" element={<Orders />} />
         </Routes>
       </div>
     </div>
