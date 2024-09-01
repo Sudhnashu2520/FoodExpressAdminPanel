bc0346b903a11c506d9e735fb874373e65c7ed4f
Sudhnashu2520 <101256221+Sudhnashu2520@users.noreply.github.com>
2024-09-02 03:30:23 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,11 +5,9 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-<<<<<<< HEAD
-export const url = 'http://foodexpressbackend-k8u1.onrender.com'
-=======
+
 export const url = 'https://foodexpressbackend-k8u1.onrender.com'
->>>>>>> 21089dcb52b72f6dab215d5f3e3d3ac7e285fcfe
+
 export const currency = '₹'
 
 export const assets ={
